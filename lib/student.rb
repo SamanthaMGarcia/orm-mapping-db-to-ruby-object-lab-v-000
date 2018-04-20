ee3a29c625416830c925648db58f79226ece3d1b
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-04-20 15:50:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,17 +57,19 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10
-    sql = <<-SQL
-      SELECT FIRST
-      FROM students
-      WHERE grade = 10;
-    SQL
+  def self.first_X_students_in_grade_10(number)
+   sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE grade = 10
+     ORDER BY students.id
+     LIMIT ?
+   SQL
 
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
+   DB[:conn].execute(sql, number).map do |row|
+     self.new_from_db(row)
+   end
+ end
 
   def save
     sql = <<-SQL
